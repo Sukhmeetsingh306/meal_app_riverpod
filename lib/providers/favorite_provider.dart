446b51fef088562d632485122917c0c140efa1bf
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-16 11:22:14 +0530

    added the way by which te riverpod must be used

---
+++ lib/providers/favorite_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../model/meals_model.dart';
+
+// state Notifier class name should always end with Notifier
+class FavoriteMealsProviderNotifier extends StateNotifier<List<MealModel>> {
+  FavoriteMealsProviderNotifier() : super([]);
+
+// in the provider add() & remove() is not allowed we have replace() it
+  void toggleMealFavoriteStatus(MealModel meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state
+          .where((mealIsPresentInList) => mealIsPresentInList.id != meal.id)
+          .toList();
+      // this is if a new meal is added to the List other meal is not replaced
+    } else {
+      // (...) this is spread operator
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+/// we have added the functionality in the <> that it should effect
+    StateNotifierProvider<FavoriteMealsProviderNotifier, List<MealModel>>(
+        (ref) {
+  return FavoriteMealsProviderNotifier();
+});
